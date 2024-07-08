a99b515dd9ab81252ebc6de8b4da7f6b98605dac
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-07-08 21:42:25 +0300

    task(stack): 1021. Remove Outer Parentheses

---
+++ src/data-structures/stack/tasks/1021-remove-outermost-parentheses.js
@@ -0,0 +1,30 @@
+import { StackMap as Stack } from '@structures/stack/StackMap.js'
+
+export function removeOuterParentheses(string) {
+  const stack = new Stack()
+  let result = ''
+
+  for (let right = 0; right < string.length; right++) {
+    const bracket = string[right]
+
+    if (bracket === '(') {
+      if (!stack.isEmpty) {
+        result += bracket
+      }
+
+      stack.push(bracket)
+    } else {
+      stack.pop()
+
+      if (!stack.isEmpty) {
+        result += bracket
+      }
+    }
+  }
+
+  return result
+}
+
+removeOuterParentheses('(()())(())')
+removeOuterParentheses('(()())(())(()(()))')
+removeOuterParentheses('()()')
